4cafeb6beb935690eac8f1ee272c5da8971df263
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-02-23 17:28:33 +0530

    understand the Validation a little

---
+++ Validation.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/health-checkup", function(req, res){
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+
+    res.send({
+        msg: "your kidney length is " +kidneyLength 
+    });
+
+   
+});
+
+//global catches all errors
+app.use(function(err, req, res, next){
+    res.json({
+        msg: "something went wrong!"
+    });
+});
+
+app.listen(8008);
