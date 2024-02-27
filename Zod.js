b291d8796308302773022f4302de559e3ee47d59
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-02-27 19:27:50 +0530

    leant zod

---
+++ Zod.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const zod =require("zod");
+
+const schema = zod.array(zod.number());
+
+//for email
+//password
+//country either IND or US
+//for these entity we can use zod object schema like this
+
+const objectSchema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+    country: zod.literal("IND").or(zod.literal("US")),
+}) 
+
+app.use(express.json());
+
+app.post("/health-checkup", function(req, res){
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys)
+    if(!response.success){
+        res.status(401).json({
+            msg: "Invalid input"
+        });
+        return;
+    }
+
+    res.send({
+        response
+    })
+});
+
+
+app.listen(8009, function(req, res){
+    console.log("Server is running on port 8009");
+});
